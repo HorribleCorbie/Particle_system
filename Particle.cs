f88be0909bc09ced7532a61bdcb200d331ba970a
Daria <pribytkovada@gmail.com>
2026-04-13 13:04:56 +0800

    Добавьте файлы проекта.

---
+++ Particle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Particle_system
+{
+    internal class Particle
+    {
+        public float Life;
+
+        public int Radius;
+        public float X; 
+        public float Y; 
+
+        public float Direction;
+        public float Speed;
+
+        public static Random rand = new Random();
+
+        public Particle()
+        {
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+
+        public void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+            int alpha = (int)(k * 255);
+
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
+    }
+}
